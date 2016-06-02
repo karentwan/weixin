7ffcdf620b1cf5aa1e6fac901a5dc5efa1473fc6
karentwan <82635922@qq.com>
2016-06-02 21:18:39 +0800

    remove

--- uweixin/test/com/weixin/test/CookieTest.java
+++
@@ -1,5 +0,0 @@
-package com.weixin.test;
-
-public class CookieTest {
-
-}
--- xuesiji/WebRoot/condition/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
- if ($_GET['floor']) {
- 	# code...
- 	$floor = $_GET['floor'];
-	// echo "$floor";
-	// echo "200";
-	echo "-1"
- }
- ?>
